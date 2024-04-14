403b23fb286694c46881a65b8893a05e19a649f2
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2024-04-14 07:58:41 +0500

    "Update app.js to use async/await with error handling and add user routes"
    This commit updates the `app.js` file to use the `asyncHandler` function for error handling with async/await. It also imports and

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 app.use(express.json({ limit: "16kb" }))
@@ -13,8 +13,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+// routes import
+import userRouter from "./routes/user.routes.js"
 
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
 export { app };
