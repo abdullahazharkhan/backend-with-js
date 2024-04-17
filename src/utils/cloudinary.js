809da7aec03a5ca5da3084d84d56f256f566f40b
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2024-04-17 21:11:44 +0500

    Update user register message and use process.env for Cloudinary api\_secret

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -4,7 +4,7 @@ import { v2 as cloudinary } from 'cloudinary';
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -20,7 +20,3 @@ const uploadOnCloudinary = async (localFilePath) => {
         return null;
     }
 }
-
-cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-    { public_id: "olympic_flag" },
-    function (error, result) { console.log(result); });
